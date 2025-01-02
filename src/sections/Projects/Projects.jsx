e04a37d9a317582411f9e3e3599aa6de2d89be99
Krishnan005K <krishnankaliyappan2005@gmail.com>
2025-01-02 17:03:29 +0530

    Update Projects.jsx

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -4,20 +4,21 @@ import freshBurger from '../../assets/fresh-burger.png';
 import hipsster from '../../assets/hipsster.png';
 import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
-
+import MockInterview from '../../assets/mock-interview.jpeg';
+import FoodDelivery from '../../assets/foodHat.jpeg';
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
+          src={MockInterview}
           link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Viberr"
-          p="Streaming App"
+          h3="Prep +"
+          p="Mock Interview App"
         />
         <ProjectCard
-          src={freshBurger}
+          src={FoodDelivery}
           link="https://github.com/Ade-mir/company-landing-page-2"
           h3="Fresh Burger"
           p="Hamburger Restaurant"
