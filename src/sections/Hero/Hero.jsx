b7bb65f01ffa746e2aa99e778aa389c0a6cfd059
Krishnan005K <krishnankaliyappan2005@gmail.com>
2025-01-02 11:20:43 +0530

    minor updates

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/Design uten navn.png';
+import heroImg from '../../assets/Hero.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
@@ -8,7 +8,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf'; // Replace with your CV file
+import CV from '../../assets/Resume Krishnan K.pdf'; // Replace with your CV file
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
