d3e68530682ce09c67283bfc714eee75dbdefb78
Krishnan005K <krishnankaliyappan2005@gmail.com>
2025-01-03 22:44:43 +0530

    updates

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -16,13 +16,13 @@ import imgDescrip from '../../assets/imgDescrip.jpeg';
 function Projects() {
   // Define project categories
   const projects = [
+    { src: passwordManager, link: "https://github.com/Krishnan005K/Password-Manager", h3: "PassManager", p: "Password Manager App", category: "Cybersecurity" },
     { src: mockInterview, link: "https://github.com/Krishnan005K/MockInterview_System", h3: "Prep +", p: "Mock Interview App", category: "Web Application" },
     { src: decoyIntruction, link: "https://github.com/Krishnan005K/Decoy-Intruction-detection-system", h3: "IntruDector", p: "File Decoy App", category: "Cybersecurity" },
     { src: foodDelivery, link: "https://github.com/Krishnan005K/Pizza-Hat", h3: "Pizza Hat", p: "Food Delivery App", category: "Web Application" },
     { src: bookReview, link: "https://github.com/Krishnan005K/BookReview-App", h3: "Boofoks", p: "Book Review App", category: "Web Application" },
     { src: taskManager, link: "https://github.com/Krishnan005K/Task-Manager", h3: "Tasker", p: "Personal Task Manager", category: "Web Application" },
     { src: imgDescrip,link:"https://github.com/Krishnan005K/Image-Desciption",h3:"ImgDescrip",p:"Image Descriptor App",category:"Other"},
-    { src: passwordManager, link: "https://github.com/Krishnan005K/Password-Manager", h3: "PassManager", p: "Password Manager App", category: "Cybersecurity" },
     { src: recipeApp, link: "https://github.com/Krishnan005K/Recipe-API", h3: "ChooseRecipe", p: "Cooking Recipe App", category: "Web Application" },
     { src: handWritten,link:"https://github.com/Krishnan005K/Handwrittien-Digit-Recognition",h3:"DigitRecog",p:"Written Digit Recog",category:"Other"},
     { src: ageGender,link:"https://github.com/Krishnan005K/Age-and-Gender-Detection",h3:"Age&Gender",p:"Age Gender Detector",category:"Other"},
