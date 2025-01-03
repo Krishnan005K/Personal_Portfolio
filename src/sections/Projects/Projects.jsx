5a3e6855d76fae999c615db6939c0475f0611c49
Krishnan005K <krishnankaliyappan2005@gmail.com>
2025-01-03 12:00:56 +0530

    added new Projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,23 +1,25 @@
 import styles from './ProjectsStyles.module.css';
 import decoyIntruction from '../../assets/decoy-intrusion.jpeg'
-import bookReview from '../../assets/book-review.jpeg'
+import bookReview from '../../assets/bookReview.jpeg'
 import ProjectCard from '../../common/ProjectCard';
-import MockInterview from '../../assets/Mock-interview.jpeg';
-import FoodDelivery from '../../assets/foodHat.jpeg';
-
+import mockInterview from '../../assets/mockInterview.jpeg';
+import foodDelivery from '../../assets/foodHat.jpeg';
+import passwordManager from '../../assets/passwordManager.jpeg';
+import taskManager from '../../assets/taskManager.jpeg';
+import recipeApp from '../../assets/recipeApp.jpeg';
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={MockInterview}
+          src={mockInterview}
           link="https://github.com/Ade-mir/company-landing-page-2"
           h3="Prep +"
           p="Mock Interview App"
         />
         <ProjectCard
-          src={FoodDelivery}
+          src={foodDelivery}
           link="https://github.com/Ade-mir/company-landing-page-2"
           h3="Fresh Burger"
           p="Hamburger Restaurant"
@@ -35,21 +37,22 @@ function Projects() {
           p="Fitness App"
         />
       </div>
+      <br/>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={MockInterview}
+          src={taskManager}
           link="https://github.com/Ade-mir/company-landing-page-2"
           h3="Prep +"
           p="Mock Interview App"
         />
         <ProjectCard
-          src={FoodDelivery}
+          src={passwordManager}
           link="https://github.com/Ade-mir/company-landing-page-2"
           h3="Fresh Burger"
           p="Hamburger Restaurant"
         />
         <ProjectCard
-          src={bookReview}
+          src={recipeApp}
           link="https://github.com/Ade-mir/company-landing-page-2"
           h3="Hipsster"
           p="Glasses Shop"
